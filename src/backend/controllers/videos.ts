c0879ddf9d6615d1f6f37b937e41c98b3439c427
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-04-07 15:10:33 +0530

    feat(u-tube/backend): Update filter logic, used or

--- src/backend/controllers/videos.ts
+++ src/backend/controllers/videos.ts
@@ -11,16 +11,25 @@ export const getAllVideos = async (req: Request, res: Response) => {
       filterInQuery === "All"
         ? {}
         : {
-            title: {
-              $regex: filterInQuery,
-              $options: "i", // insensitive
-            },
+            $or: [
+              {
+                title: {
+                  $regex: filterInQuery,
+                  $options: "i", // insensitive
+                },
+              },
+              {
+                description: {
+                  $regex: filterInQuery,
+                  $options: "i", // insensitive
+                },
+              },
+            ],
           };
-    // We've to use regex to find it in title, so it will search in title is filterQuery present in that, if we don't use regex it will try to finx exact match to the query, means title should be same as queryFilter.
+    // We've to use regex to find it in title, so it will search in title is filterQuery present in that, if we don't use regex it will try to finx exact match to the query, means title should be same as queryFilter. And same with description. We've used $or so it will return the video if at title or description any one contains the filterQuery
     const videos = await Video.find({ ...filter }).populate("user");
     return res.status(200).send({ Success: true, videos });
   } catch (err) {
-    console.log(err);
     return res
       .status(500)
       .send({ Success: false, message: "Internal Server Error" });

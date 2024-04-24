e9d9b802fc4cffc4e488c670a06f9871d0da24ee
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-04-24 09:44:00 +0530

    feat(u-tube/frontend): Protect profile route, redirect user back to home page if not logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,21 +6,21 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 // Middleware runs with every request, here we will protect the routes
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
   const token = request.cookies.get("token")?.value;
-  console.log(process.env.MONGODB_URI);
-  event.waitUntil(
-    fetch(`http://localhost:3001/api/auth/is_token_valid?token=${token}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        if (!data.Success) {
-          console.log("Heyy");
-          return NextResponse.redirect(new URL("/login", request.url));
-        }
-      })
-      .catch((err) => console.log("error", err))
+
+  const response = await fetch(
+    `http://localhost:3001/api/auth/is_token_valid?token=${token}`
   );
+
+  const data = await response.json();
+  console.log(data);
+
+  // if success is false refirect to homepage again
+  if (!data?.Success) {
+    return NextResponse.redirect(new URL("http://localhost:3000", request.url));
+  }
+
+  // If token is valid continue
+  return NextResponse.next();
 }
 
 export const config = {
